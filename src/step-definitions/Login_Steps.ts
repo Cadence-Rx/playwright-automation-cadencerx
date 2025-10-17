d4c6cd519f80166ce6a995b892b28d216e65b4eb
Latunji Asgill <lasgill@cadencerx.com>
2025-10-17 13:21:51 -0400

    i guess playwright doesn't like underscore in folder structure

---
+++ src/step-definitions/Login_Steps.ts
@@ -0,0 +1,27 @@
+import { defineStep as And, Given, When, Then} from '@cucumber/cucumber';
+import { Browser } from 'playwright';
+import { Page } from 'playwright';
+import { LoginPage } from '../page-objects/LoginPage';
+
+let browser: Browser;
+let contexts: any;
+let page: Page; 
+
+const loginPage = new LoginPage();
+
+Given('I navigate to the Opus application', async () => {
+    await loginPage.navigateToOpusLoginPage();
+});
+
+When('I enter a valid username {string} and password {string}', async () => {
+    await loginPage.enterUsername('valid_username');
+    await loginPage.enterPassword('valid_password');
+});
+
+And('I click the login button', async () => {   
+    await loginPage.clickLoginButton();
+});
+
+Then('I should be logged in successfully', async () => {
+    await loginPage.verifyLoginSuccess();
+}); 
