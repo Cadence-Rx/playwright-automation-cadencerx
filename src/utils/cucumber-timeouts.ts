11fa97dbb791ce5806e1dd5dfbe0158dd69490ad
Latunji Asgill <lasgill@cadencerx.com>
2025-10-22 14:27:51 -0400

    adding playwright timeouts

--- src/utils/cucumber-timeouts.ts
+++ src/utils/cucumber-timeouts.ts
@@ -1,4 +1,10 @@
 import { setDefaultTimeout } from '@cucumber/cucumber';
 
+//Load env variables from .env file
+import {config as loadEnv} from "dotenv";
+const env = loadEnv({path: './env/.env'});
+
+const customTime = parseInt(env.parsed?.CUCUMBER_STEP_TIMEOUT || '60000');
+
 // Set default timeout for Cucumber steps to 60 seconds
-setDefaultTimeout(60 * 1000);   
+setDefaultTimeout(customTime);
