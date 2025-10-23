52d1531792b8e6133e06201b18c9f9adff373f69
Latunji Asgill <lasgill@cadencerx.com>
2025-10-23 14:20:21 -0400

    turning on headless mode

--- src/step-definitions/Login_Steps.ts
+++ src/step-definitions/Login_Steps.ts
@@ -11,15 +11,12 @@ Given('I navigate to the Opus application', async () => {
 });
 
 When('I enter a valid username {string} and password {string}', async (email: string, pwd: string) => {
-    const loginPage = getLoginPage();
-    await loginPage.enterUsername(email);
-    await loginPage.enterPassword(pwd);
+    await getLoginPage().enterUsername(email);
+    await getLoginPage().enterPassword(pwd);
 });
 
 And('I click on the login button', async function (this: CucumberWorld) {   
-    const loginPage = getLoginPage();
-    await loginPage.clickLoginButton();
-    // await this.basePage.waitAndClickByLocator('input#loginButton');
+    await getLoginPage().clickLoginButton();
 });
 
 Then('I should be logged in successfully', async () => {
