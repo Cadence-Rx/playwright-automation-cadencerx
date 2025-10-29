831b9ed0cc81a4c32a00b4d8767e85e2627e48e1
Latunji Asgill <lasgill@cadencerx.com>
2025-10-29 14:54:20 -0400

    15342: adding logic to alos make sure that the authorizat tab on opus dash is active

--- src/page-objects/AuthorizationTabOpusDashboardPage.ts
+++ src/page-objects/AuthorizationTabOpusDashboardPage.ts
@@ -3,16 +3,22 @@ import { BasePage } from './base/BasePage';
 
 export class AuthorizationTabOpusDashboardPage extends BasePage {
     readonly authorizationTab: Locator;
+    readonly myAuthorizationTab: Locator;
 
     constructor(page: Page) {
         super();
-        this.authorizationTab = page.locator("a.btn.btn-white.border.btn-sm.authRefreshButton.authRefreshButtonMy");
+        this.authorizationTab = page.locator("a[href='#transaction-details']");
+        this.myAuthorizationTab = page.locator("a.btn.btn-white.border.btn-sm.authRefreshButton.authRefreshButtonMy");
     }
 
     public async verifyAuthorizationTab(): Promise<void> {
         await expect(this.authorizationTab, 'My Authorization button is visible!').toBeVisible();
-        await expect(this.authorizationTab).toHaveText('My Authorizations');
+        await expect(this.authorizationTab).toHaveClass('tab active');
+        await expect(this.authorizationTab).toHaveText('Authorizations');
     }
 
-
+    public async verifyMyAuthorizationTab(): Promise<void> {
+        await expect(this.myAuthorizationTab, 'My Authorization button is visible!').toBeVisible();
+        await expect(this.myAuthorizationTab).toHaveText('My Authorizations');
+    }   
 }
