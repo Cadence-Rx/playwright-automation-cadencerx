96cdd5e86362c3f1aacbde6244b840e34bfa68d5
Latunji Asgill <lasgill@cadencerx.com>
2025-11-05 09:45:22 -0500

    15428: adding screenshot to last verification step

--- src/step-definitions/patient-profile/PatientNotes_Steps.ts
+++ src/step-definitions/patient-profile/PatientNotes_Steps.ts
@@ -1,6 +1,8 @@
 import { defineStep as And, Given, When, Then} from '@cucumber/cucumber';
 import { pageFixture } from './../hooks/browserContextFixture';
 import { PatientNotesPage } from '../../page-objects/patient-profile/PatientNotesPage';
+import { CucumberWorld } from '../world/CucumberWorld';
+import { ScreenshotUtils } from '../../utils/screenshot-utils';
 
 const getPatientNotesPage = () => new PatientNotesPage(pageFixture.page);
 
@@ -21,6 +23,8 @@ And('I click on the Save Note button', async () => {
     await getPatientNotesPage().clickSaveNoteButton();
 });
 
-Then('I should see the newly added note in the Patient Notes tab', async () => {
+Then('I should see the newly added note in the Patient Notes tab', async function (this: CucumberWorld) {
     await getPatientNotesPage().verifyNewlyAddedNoteIsVisible("Entering a test note for automation");
+    const screenshot = await ScreenshotUtils.takeScreenshot(pageFixture.page, 'newly-added-patient-note');
+    await this.attach(screenshot, 'image/png');
 });
